89158228c0d65d931e440e760537b1a0cd739c18
Rajat <rajat22sethi@gmail.com>
2017-08-12 15:16:56 -0400

    0.0.1 Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## angular2-seed
+
+This repo is a woking model of how to connect oData service to angular 2/4.
+
+Based on blog post: https://www.codementor.io/antonselin/consume-odata-service-restful-angular-2-uj1i6szsw
---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { AppComponent } from './app.component';
+import { GithubService } from './github/shared/github.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { RepoBrowserComponent } from './github/repo-browser/repo-browser.component';
+import { RepoListComponent } from './github/repo-list/repo-list.component';
+import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { ContactComponent } from './contact/contact.component';
+import { ODataComponent } from './odata/odata.component';
+import { provideODataService } from './odata/odata.serviceProvider';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    RepoBrowserComponent,
+    RepoListComponent,
+    RepoDetailComponent,
+    HomeComponent,
+    ContactComponent,
+    ODataComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: true })
+  ],
+  providers: [
+    GithubService,
+    provideODataService("http://services.odata.org/V3/(S(pq1lpmgz0kuok05ubqtx1c2g))/OData/OData.svc/")
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+
+}
